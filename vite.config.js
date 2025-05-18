94de16f594c5ef1a6265536c0d5c271e40160697
nataliadesiany <19623204@std.stei.itb.ac.id>
2025-05-19 03:21:05 +0700

    Afaa

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // atau bisa juga '/' untuk root
+  base: './',
   plugins: [react()],
+  preview: {
+    port: 8081,
+    host: '0.0.0.0',
+    allowedHosts: ['www.alchemyrecipes.online'] // Tambahkan ini
+  }
 })
